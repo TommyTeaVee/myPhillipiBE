9a357c987a6ac297ace366a799de6a1e9e1b090f
Thomas Fihla <fihlatv@gmail.com>
2022-08-01 15:28:40 +0200

    host cors2

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ app.use(cors({
 }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header('Access-Control-Allow-Origin: *');
+res.header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS');
+res.header("Access-Control-Allow-Headers: X-Requested-With");
+  
+  
   next();
 });
 
